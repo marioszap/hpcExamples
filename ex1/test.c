d08df4db3556f6ece44be411944a937c50395b7d
Gamma-Pan <yiorgospanag@gmail.com>
2023-12-18 14:13:09 +0200

    new branch

--- ex1/test.c
+++ ex1/test.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include "omp.h"
+
+
+
+static long num_steps = 1e4;
+double step;
 
 int main() {
-    char* s = "hello world";
-    printf("%s", s);
-    return 0;
+    int i;
+    double x, pi, sum = 0.;
+    step = 1./(double) num_steps;
+
+    for(i=0; i<num_steps; i++){
+        x = (i+0.5) * step;
+        sum = sum + 4./(1. +x*x);
+    }
+    pi = step*sum;
+
+    printf("%f \n", pi);
+
 }
